8aba548bab8f1c59cee3de61f7c290d60627ce8d
maicFir <mcodes@163.com>
2024-12-13 19:42:38 +0800

    add ignoreBuildError

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     reactStrictMode: false,
+    typescript: {
+        ignoreBuildErrors: true, // 忽略类型检查错误
+     },
     
 };
 
